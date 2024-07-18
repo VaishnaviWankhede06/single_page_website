6549e124aeb64f03beb130591141167594aabe22
VaishnaviWankhede06 <vaishnviwankhede10@gmail.com>
2024-07-18 09:24:30 +0530

    18-7-24

---
+++ dom.js
@@ -0,0 +1,9 @@
+function changeImage(){
+    document.getElementById("img-id").src='card2image.jpg';
+}
+function changeImage2(){
+    document.getElementById("img-id").src='./Images/tabs-2.jpg';
+}
+function changeImage3(){
+    document.getElementById("img-id").src='./Images/tabs-3.jpg';
+}
